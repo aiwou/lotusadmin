fcafeb04879e5feff457b2596122abfa7caf33bb
wenhainan <whndeweilai@163.com>
2017-12-01 13:31:06 +0800

    database.php

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '122.152.192.48',
+    'hostname'        => 'localhost',
     // 数据库名
     'database'        => 'lotus',
     // 用户名
