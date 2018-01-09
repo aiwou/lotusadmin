d4bd3a156a3a997d5ac05689f12b4808610bf9d4
wenhainan <whndeweilai@163.com>
2018-01-09 11:02:23 +0800

    文件上传bug修复

--- application/admin/controller/FileSystem.php
+++ application/admin/controller/FileSystem.php
@@ -17,7 +17,7 @@ class FileSystem extends Main
              $file = $this->request->file('file');//file是传文件的名称，这是webloader插件固定写入的。因为webloader插件会写入一个隐藏input，不信你们可以通过浏览器检查页面
              $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
              Db::name('file')
-                ->insert(['size'=>$info->getSize(),'url'=> '\public' . DS . 'uploads'.DS.$info->getSaveName()]);
+                ->insert(['size'=>$info->getSize(),'url'=> '/public' . DS . 'uploads'.DS.$info->getSaveName()]);
              $this->success('上传成功');
         }else{
             return $this->fetch();
