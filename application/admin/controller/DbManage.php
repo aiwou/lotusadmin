60e60d9e449dc357e51a3c9d2ef34c8ebb8479b2
wenhainan <whndeweilai@163.com>
2018-11-18 00:05:46 +0800

    数据库功能完善

--- application/admin/controller/DbManage.php
+++ application/admin/controller/DbManage.php
@@ -4,18 +4,35 @@ namespace app\admin\controller;
 use think\Db;
 use org\Backup;
 
+
 const PREFIX = 'lotus_';
 
 class DbManage extends Main
 {
-  //数据库维护页面展示 
-  // function index(){
-  //   $db_names =  Db::getTables();
-  //   $count = count($db_names);
-  //   $this->assign('db_names',$db_names);
-  //   $this->assign('count',$count);
-  //   return $this->fetch();
-  // }
+
+    public $config = '';                                                        //相关配置
+    public $model = '';                                                         //实例化一个model
+    public $content;                                                            //内容
+    public $dbName = '';                                                        //数据库名
+    public $dir_sep = '/';                                                      //路径符号
+
+
+    //初始化数据
+    function _initialize() {
+        parent::_initialize();
+        header("Content-type: text/html;charset=utf-8");
+        set_time_limit(0);                                                      //不超时
+        ini_set('memory_limit','500M');
+        $this->config = array(
+            'path' => config('database')['db_backup'],                                           //备份文件存在哪里
+            'isCompress' => 0,                                                  //是否开启gzip压缩      【未测试】
+            'isDownload' => 0                                                   //备份完成后是否下载文件 【未测试】
+        );
+        $this->dbName = config('database')['database'];                                           //当前数据库名称
+        //$sql = 'set interactive_timeout=24*3600';                             //空闲多少秒后 断开链接
+        //$this->model>execute($sql);
+    }
+
 
      /**
      * 数据表列表
@@ -33,127 +50,225 @@ class DbManage extends Main
                 $total += $v['Data_length'] + $v['Index_length'];
             }
         }
-        if (session('?backup_config.path')) {
-            //备份完成，清空缓存
-            @unlink(session('backup_config.path') . 'backup.lock');
-            session('backup_tables', null);
-            session('backup_file', null);
-            session('backup_config', null);
-        }
         $this->assign('list', $list);
         $this->assign('total', format_bytes($total));
         $this->assign('tableNum', count($list));
         return $this->fetch();
     }
-
-
-    //
-    public function backup(){
-
+    /* -
+     * +------------------------------------------------------------------------
+     * * @ 备份数据 { 备份每张表、视图及数据 }
+     * +------------------------------------------------------------------------
+     * * @ $tables 需要备份的表数组
+     * +------------------------------------------------------------------------
+     */
+    function backup($tables) {
+        if (empty($tables))
+            $this->error('没有需要备份的数据表!');
+        $this->content  = '/* This file is created by MySQLReback ' . date('Y-m-d H:i:s') . ' */';
+        $this->content .= "\r\n SET FOREIGN_KEY_CHECKS=0;"; 
+        foreach ($tables as $i => $table) {
+            $table = $this->backquote($table);                                  //为表名增加 ``
+            $tableRs = Db::query("SHOW CREATE TABLE {$table}");       //获取当前表的创建语句
+            if (!empty($tableRs[0]["Create View"])) {
+                $this->content .= "\r\n /* 创建视图结构 {$table}  */";
+                $this->content .= "\r\n DROP VIEW IF EXISTS {$table};/* MySQLReback Separation */ \r\n " . $tableRs[0]["Create View"] . ";/* MySQLReback Separation */";
+            }
+            if (!empty($tableRs[0]["Create Table"])) {
+                $this->content .= "\r\n /* 创建表结构 {$table}  */";
+                $this->content .= "\r\n DROP TABLE IF EXISTS {$table};/* MySQLReback Separation */ \r\n " . $tableRs[0]["Create Table"] . ";/* MySQLReback Separation */";
+                $tableDateRow =  Db::query("SELECT * FROM {$table}");
+                $valuesArr = array();
+                $values = '';
+                if (false != $tableDateRow) {
+                    foreach ($tableDateRow as &$y) {
+                        foreach ($y as &$v) {
+                           if ($v=='')                                  //纠正empty 为0的时候  返回tree
+                                $v = 'null';                                    //为空设为null
+                            else
+                                $v = "'" . $v . "'";       //非空 加转意符
+                        }
+                        $valuesArr[] = '(' . implode(',', $y) . ')';
+                    }
+                }
+                $temp = $this->chunkArrayByByte($valuesArr);
+                if (is_array($temp)) {
+                    foreach ($temp as $v) {
+                        $values = implode(',', $v) . ';/* MySQLReback Separation */';
+                        if ($values != ';/* MySQLReback Separation */') {
+                            $this->content .= "\r\n /* 插入数据 {$table} */";
+                            $this->content .= "\r\n INSERT INTO {$table} VALUES {$values}";
+                        }
+                    }
+                }
+//                dump($this->content);
+//                exit;
+            }
+        }
+        if (!empty($this->content)) {
+            $fileName =  $this->setFile();
+        }
+        $this->success('备份成功,文件:<br>'.$fileName);
     }
 
 
-
-    /**
-     * 数据备份
-    */
-    public function export($tables = null, $id = null, $start = null)
-    {
-        //防止备份数据过程超时
-        function_exists('set_time_limit') && set_time_limit(0);
-
-        if(IS_POST && !empty($tables) && is_array($tables)){ //初始化
-            $path = tpCache('global.web_sqldatapath');
-            $path = !empty($path) ? $path : config('DATA_BACKUP_PATH');
-            $path = trim($path, '/');
-            if(!empty($path) && !is_dir($path)){
-                mkdir($path, 0755, true);
-            }
-
-            //读取备份配置
-            $config = array(
-                'path'     => realpath($path) . DS,
-                'part'     => config('DATA_BACKUP_PART_SIZE'),
-                'compress' => config('DATA_BACKUP_COMPRESS'),
-                'level'    => config('DATA_BACKUP_COMPRESS_LEVEL'),
-            );
-            //检查是否有正在执行的任务
-            $lock = "{$config['path']}backup.lock";
-            if(is_file($lock)){
-                return json(array('info'=>'检测到有一个备份任务正在执行，请稍后再试！', 'status'=>0, 'url'=>''));
+    private function chunkArrayByByte($array, $byte = 5120) {
+        $i = 0;
+        $sum = 0;
+        $return = array();
+        foreach ($array as $v) {
+            $sum += strlen($v);
+            if ($sum < $byte) {
+                $return[$i][] = $v;
+            } elseif ($sum == $byte) {
+                $return[++$i][] = $v;
+                $sum = 0;
             } else {
-                //创建锁文件
-                file_put_contents($lock, NOW_TIME);
+                $return[++$i][] = $v;
+                $i++;
+                $sum = 0;
             }
+        }
+        return $return;
+    }
 
-            //检查备份目录是否可写
-            if(!is_writeable($config['path'])){
-                return json(array('info'=>'备份目录不存在或不可写，请检查后重试！', 'status'=>0, 'url'=>''));
-            }
-            session('backup_config', $config);
+        /* -
+     * +------------------------------------------------------------------------
+     * * @ 给字符串添加 ` `
+     * +------------------------------------------------------------------------
+     * * @ $str 字符串
+     * +------------------------------------------------------------------------
+     * * @ 返回 `$str`
+     * +------------------------------------------------------------------------
+     */
+    private function backquote($str) {
+        return "`{$str}`";
+    }
 
-            //生成备份文件信息
-            $file = array(
-                'name' => date('Ymd-His', $_SERVER['REQUEST_TIME']),
-                'part' => 1,
-                'version' => getCmsVersion(),
-            );
-            session('backup_file', $file);
-            //缓存要备份的表
-            session('backup_tables', $tables);
-            //创建备份文件
-            $Database = new Backup($file, $config);
-            if(false !== $Database->create()){
-                $speed = (floor((1/count($tables))*10000)/10000*100).'%';
-                $tab = array('id' => 0, 'start' => 0, 'speed'=>$speed, 'table'=>$tables[0]);
-                return json(array('tables' => $tables, 'tab' => $tab, 'info'=>'初始化成功！', 'status'=>1, 'url'=>''));
-            } else {
-                return json(array('info'=>'初始化失败，备份文件创建失败！', 'status'=>0, 'url'=>''));
+    /* -
+     * +------------------------------------------------------------------------
+     * * @ 获取数据库的所有表
+     * +------------------------------------------------------------------------
+     * * @ $dbName  数据库名称
+     * +------------------------------------------------------------------------
+     */
+    private function getTables($dbName = '') {
+        if (!empty($dbName)) {
+            $sql = 'SHOW TABLES FROM ' . $dbName;
+        } else {
+            $sql = 'SHOW TABLES ';
+        }
+        $result = Db::query($sql);
+        $info = array();
+        foreach ($result as $key => $val) {
+            $info[$key] = current($val);
+        }
+        return $info;
+    }
+
+        /* -
+     * +------------------------------------------------------------------------
+     * * @ 把数据写入磁盘
+     * +------------------------------------------------------------------------
+     */
+    private function setFile() {
+        $recognize = '';
+        $recognize = $this->dbName;
+        $fileName = $this->trimPath($this->config['path'] . $this->dir_sep . $recognize . '_' . date('Ymd') . '_' . mt_rand(1000, 9999) . '.sql');
+        $path = $this->setPath($fileName);
+        if ($path !== true) {
+            $this->error("无法创建备份目录目录 '$path'");
+        }
+        if ($this->config['isCompress'] == 0) {
+            if (!file_put_contents($fileName, $this->content, LOCK_EX)) {
+                $this->error('写入文件失败,请检查磁盘空间或者权限!');
             }
-        } elseif (IS_GET && is_numeric($id) && is_numeric($start)) { //备份数据
-            $tables = session('backup_tables');
-            //备份指定表
-            $Database = new Backup(session('backup_file'), session('backup_config'));
-            $start  = $Database->backup($tables[$id], $start);
-            if(false === $start){ //出错
-                return json(array('info'=>'备份出错！', 'status'=>0, 'url'=>''));
-            } elseif (0 === $start) { //下一表
-                if(isset($tables[++$id])){
-                    $speed = (floor((($id+1)/count($tables))*10000)/10000*100).'%';
-                    $tab = array('id' => $id, 'start' => 0, 'speed' => $speed, 'table'=>$tables[$id]);
-                    return json(array('tab' => $tab, 'info'=>'备份完成！', 'status'=>1, 'url'=>''));
-                } else { //备份完成，清空缓存
-                    /*自动覆盖安装目录下的eyoucms.sql*/
-                    $install_time = DEFAULT_INSTALL_DATE;
-                    $constsant_path = APP_PATH.MODULE_NAME.'/conf/constant.php';
-                    if (file_exists($constsant_path)) {
-                        require_once($constsant_path);
-                        defined('INSTALL_DATE') && $install_time = INSTALL_DATE;
-                    }
-                    $install_path = ROOT_PATH.'install_'.$install_time;
-                    if (is_dir($install_path) && file_exists($install_path)) {
-                        $srcfile = session('backup_config.path').session('backup_file.name').'-'.session('backup_file.part').'-'.session('backup_file.version').'.sql';
-                        $dstfile = $install_path.'/eyoucms.sql';
-                        @copy($srcfile, $dstfile);
-                    }
-                    /*--end*/
-                    unlink(session('backup_config.path') . 'backup.lock');
-                    session('backup_tables', null);
-                    session('backup_file', null);
-                    session('backup_config', null);
-                    return json(array('info'=>'备份完成！', 'status'=>1, 'url'=>''));
+        } else {
+            if (function_exists('gzwrite')) {
+                $fileName .= '.gz';
+                if ($gz = gzopen($fileName, 'wb')) {
+                    gzwrite($gz, $this->content);
+                    gzclose($gz);
+                } else {
+                    $this->error('写入文件失败,请检查磁盘空间或者权限!');
                 }
             } else {
-                $rate = floor(100 * ($start[0] / $start[1]));
-                $speed = floor((($id+1)/count($tables))*10000)/10000*100 + ($rate/100);
-                $tab  = array('id' => $id, 'start' => $start[0], 'speed' => $speed.'%', 'table'=>$tables[$id]);
-                return json(array('tab' => $tab, 'info'=>"正在备份...({$rate}%)", 'status'=>1, 'url'=>''));
+                $this->error('没有开启gzip扩展!');
             }
-
-        } else {//出错
-            return json(array('info'=>'参数有误', 'status'=>0, 'url'=>''));
+        }
+        if ($this->config['isDownload']) {
+            $this->downloadFile($fileName);
+        }
+        return $fileName;
+    }
+    private function trimPath($path) {
+        return str_replace(array('/', '\\', '//', '\\\\'), $this->dir_sep, $path);
+    }
+    private function setPath($fileName) {
+        $dirs = explode($this->dir_sep, dirname($fileName));
+        $tmp = '';
+        foreach ($dirs as $dir) {
+            $tmp .= $dir . $this->dir_sep;
+            if (!file_exists($tmp) && !@mkdir($tmp, 0777))
+                return $tmp;
+        }
+        return true;
+    }
+     /* -
+     * +------------------------------------------------------------------------
+     * * @ 下载备份文件
+     * +------------------------------------------------------------------------
+     */
+    function downloadBak() {
+        $file_name = $_GET['file'];
+        $file_dir = $this->config['path'];
+        if (!file_exists($file_dir . "/" . $file_name)) { //检查文件是否存在
+            return false;
+            exit;
+        } else {
+            $file = fopen($file_dir . "/" . $file_name, "r"); // 打开文件
+            // 输入文件标签
+            header('Content-Encoding: none');
+            header("Content-type: application/octet-stream");
+            header("Accept-Ranges: bytes");
+            header("Accept-Length: " . filesize($file_dir . "/" . $file_name));
+            header('Content-Transfer-Encoding: binary');
+            header("Content-Disposition: attachment; filename=" . $file_name);  //以真实文件名提供给浏览器下载
+            header('Pragma: no-cache');
+            header('Expires: 0');
+            //输出文件内容
+            echo fread($file, filesize($file_dir . "/" . $file_name));
+            fclose($file);
+            exit;
         }
     }
 
-  
+    function backup_list(){
+        $list = $this->MyScandir('data/sqldata');
+        $this->assign('list',$list);
+        return $this->fetch();
+    }
+
+    /* -
+     * +------------------------------------------------------------------------
+     * * @ 获取 目录下文件数组
+     * +------------------------------------------------------------------------
+     * * @ $FilePath 目录路径
+     * * @ $Order    排序
+     * +------------------------------------------------------------------------
+     * * @ 获取指定目录下的文件列表，返回数组
+     * +------------------------------------------------------------------------
+     */
+    private function MyScandir($FilePath = './', $Order = 0) {
+        $FilePath = opendir($FilePath);
+        while ($filename = readdir($FilePath)) {
+            if($filename!=='.'&&$filename!=='..'){
+                $fileArr[] = $filename;
+            }
+        }
+        $Order == 0 ? sort($fileArr) : rsort($fileArr);
+        return $fileArr;
+    }
+
+
 }
